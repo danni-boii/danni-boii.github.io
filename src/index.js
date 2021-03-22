85ed357ca78f912f720b894e26372473d69ac056
ylf050189 <ylf050189@gmail.com>
2021-03-23 03:00:36 +0800

    Reinstalled with templete

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import WeatherApp from './WeatherApp';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(<WeatherApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
